3ff195812e10ced14e4cc2debabbdc62cf5576a2
Maximilian Edmunds <edmunds.m@etoncollege.org.uk>
2021-11-30 19:03:30 +0000

    Print statements fixed

--- StandardAlgorithmsNew/main.swift
+++ StandardAlgorithmsNew/main.swift
@@ -32,36 +32,32 @@ print("""
 
     Output : \(bubbleOutput)
       
-""")
 
-print("""
-      Insertion sort is a simple sorting algorithm that builds the final sorted array one item at a time.
 
-      The time complexity is O(n^2)
+    Insertion sort is a simple sorting algorithm that builds the final sorted array one item at a time.
 
-      The space complexity is O(1)
+    The time complexity is O(n^2)
 
-      Input : [1,5,899,23,52,1,345,211,52,1,14,5,1,86,8]
+    The space complexity is O(1)
 
-      Output : \(insertionOutput)
-      
-""")
+    Input : [1,5,899,23,52,1,345,211,52,1,14,5,1,86,8]
 
-print("""
-      
-      Merge sort is a sorting technique that sequences data by continuously splitting and merging items in the list while ordering them.
-      
-      The time complexity is O(nLogn)
-      
-      The space complexity is O(n)
-      
-      Input : [1,5,899,23,52,1,345,211,52,1,14,5,1,86,8]
-      
-      Output : \(mergeOutput)
-      
-""")
+    Output : \(insertionOutput)
 
-print("""
+
+
+    Merge sort is a sorting technique that sequences data by continuously splitting and merging items in the list while ordering them.
+
+    The time complexity is O(nLogn)
+
+    The space complexity is O(n)
+
+    Input : [1,5,899,23,52,1,345,211,52,1,14,5,1,86,8]
+
+    Output : \(mergeOutput)
+      
+
+
     Quicksort is a divide-and-conquer algorithm. It works by selecting a 'pivot' element from the array and partitioning the other elements into two sub-arrays, according to whether they are less than or greater than the pivot.
           
     The time complexity is O(n^2)
@@ -72,9 +68,8 @@ print("""
           
     Output : \(quickOutput)
       
-""")
 
-print("""
+
     Linear search is a searching algorithm that filters through every item in a list until it either finds its target or reaches the end of the list.
           
     The time complexity is O(n)
@@ -93,9 +88,8 @@ print("""
           
     Output : \(linearOutput2)
       
-""")
 
-print("""
+
     Bubble sort is a sorting algorithm that repeatedly steps through the list, compares adjacent elements and swaps them if they are in the wrong order.
       
     The time complexity is O(n^2)
